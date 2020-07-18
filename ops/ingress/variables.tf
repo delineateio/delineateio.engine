f67916bf6cb8829913fe9c25fd3adf1e11964de2
Jonathan Fenwick <jonathan.fenwick@delineate.io>
2020-07-18 20:54:48 +0100

    Reafactor to simplify and improve maintenance (#16)
    The following changed were made:
    
    - Renaming of GCP related env aligned to Terraform docs
    - Removed need to pass in certain env to Terraform
    - Bumped Terraform version from 0.12.26 to 0.12.28
    - Simplified CircleCI config by re3moveing some steps
    - Parameterised the CircleCI build image names
    - Added some scripts for correctly configuring the desktop
    - Made any changes required to the VM provisioning

--- ops/ingress/variables.tf
+++ ops/ingress/variables.tf
@@ -1,18 +1,3 @@
-# GCP project ID
-variable "gcp_project" {
-  type = string
-}
-
-# GCP default region
-variable "gcp_region" {
-  type = string
-}
-
-# GCP default zone
-variable "gcp_zone" {
-  type = string
-}
-
 # Cloudflare Zone
 variable "cloudflare_zone" {
   type = string
