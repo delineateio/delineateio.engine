b04aad7593902cf7e57af6dd6cb9cf03d48729a0
Jonathan Fenwick <jonathan.fenwick@delineate.io>
2020-07-27 22:44:42 +0000

    Refactor of terraform folder structure and scripts
    * Added the container scanning API to the bootstrap script
    * Restructured the ops folder to better group cloud terraform scripts
    * Added validation for component name and type parameters into the services/dev scripts
    * Added a new script to enable reconnectivity to dev k8s after being recreated
    
    The secrets were rebaselined after checking that the changes were only caused by
    the restructuring of the ./ops folder.

---
+++ dev/services/connect.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+###################################################################
+# Script Name	: connect.sh
+# Description	: Connect kubectl to the remote 'dev` clsuter',
+#               : useful particularly after the cloud environment
+#               : has been rebuilt.
+# Author       	: Jonathan Fenwick
+# Email         : jonathan.fenwick@delineate.io
+###################################################################
+
+# Removes previous context
+kubectl config delete-context dev
+
+# Retrieves the credentials from GCP
+gcloud container clusters get-credentials "${GOOGLE_CLUSTER_NAME}"
+
+# Rename the newly added context
+kubectl config rename-context "$(kubectl config current-context)" dev
