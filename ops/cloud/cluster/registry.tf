a1a1b7eda61a7ba6e7df3e5a6dcf70c3caaf0fe9
Jonathan Fenwick <jonathan.fenwick@delineate.io>
2020-08-01 15:10:58 +0100

    Fix issue encountered in Database commit (424dbb1) on the Workload SA (#35)
    The primary purpose of this commit is to fix an issue encountered in a previous commit whereby the role assignment to the 'workload' service account which is used as the workload identity of the pods had not been implemented. When encountered it was possible to manually assign the roles in the GCP console to prove the fix.
    
    The roles that are required by the pod are as follows. Without these the pods failed to start cleanly as the permissions where denied when starting the SQL Proxy.
    
    Cloud SQL Client
    Logs Writer
    Monitoring Metric Writer
    Monitoring Viewer

---
+++ ops/cloud/cluster/registry.tf
@@ -0,0 +1,44 @@
+# Creates the service account to use when pulling images
+# https://www.terraform.io/docs/providers/google/r/google_service_account.html
+resource "google_service_account" "registry_service_account" {
+  account_id   = "registry"
+  display_name = "Registry image pull service account"
+  description  = "Service account for pulling images from GCR to GKE"
+}
+
+resource "google_project_iam_binding" "registry_iam_binding" {
+  role = "roles/storage.objectViewer"
+  members = [
+    "serviceAccount:${google_service_account.registry_service_account.email}"
+  ]
+}
+
+# Gets a service account key
+# https://www.terraform.io/docs/providers/google/r/google_service_account_key.html
+resource "google_service_account_key" "registry_key" {
+  service_account_id = google_service_account.registry_service_account.name
+  public_key_type    = "TYPE_X509_PEM_FILE"
+}
+
+# Creates the required image pull Secret
+# https://www.terraform.io/docs/providers/kubernetes/r/secret.html
+resource "kubernetes_secret" "registry_secret" {
+  metadata {
+    name = google_service_account.registry_service_account.account_id
+  }
+
+  data = {
+    ".dockerconfigjson" = jsonencode({
+      "auths" : {
+        "https://${var.gcp_registry}/${data.google_client_config.context.project}" : {
+          email    = google_service_account.registry_service_account.email
+          username = "_json_key"
+          password = trimspace(base64decode(google_service_account_key.registry_key.private_key))
+          auth     = base64encode(join(":", ["_json_key", base64decode(google_service_account_key.registry_key.private_key)]))
+        }
+      }
+    })
+  }
+
+  type = "kubernetes.io/dockerconfigjson"
+}
