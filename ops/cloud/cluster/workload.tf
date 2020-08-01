9bc07406e527cdec00991a3942f3e8c11fe58a1e
Jonathan Fenwick <jonathan.fenwick@delineate.io>
2020-08-01 13:57:26 +0000

    Fix issue encountered in Database commit (424dbb1) on the Workload SA
    The primary purpose of this commit is to fix an issue encountered in a previous commit
    whereby the role assignment to the 'workload' service account which is used as the workload
    identity of the pods had not been implemented.  When encoutered it was possible to manually
    assign the roles in the GCP console to prove the fix.
    
    The roles that are required by the pod are as follows.  Without these the pods failed to start
    cleanly as the permissions where denied when starting the SQL Proxy
    
    * Cloud SQL Client
    * Logs Writer
    * Monitoring Metric Writer
    * Monitoring Viewer

---
+++ ops/cloud/cluster/workload.tf
@@ -0,0 +1,57 @@
+# TODO: Workload identity should ultimately be managed INSIDE the service
+# this will enable least priviledge to be implemented for each pod.  At the
+# moment all pods would share the same priviledges
+
+# Creates the service account to use when pulling images
+# https://www.terraform.io/docs/providers/google/r/google_service_account.html
+resource "google_service_account" "workload_service" {
+  account_id   = "workload"
+  display_name = "Workload identity service account"
+  description  = "Service account used for running workloads in the app cluster"
+}
+
+# Binds the roles to the workload GSA
+# https://www.terraform.io/docs/providers/google/r/google_project_iam.html
+resource "google_project_iam_binding" "workload_iam_sql" {
+  role = "roles/cloudsql.client"
+  members = [
+    "serviceAccount:${google_service_account.workload_service.email}"
+  ]
+}
+resource "google_project_iam_binding" "workload_iam_log_writer" {
+  role = "roles/logging.logWriter"
+  members = [
+    "serviceAccount:${google_service_account.workload_service.email}"
+  ]
+}
+resource "google_project_iam_binding" "workload_iam_metric_writer" {
+  role = "roles/monitoring.metricWriter"
+  members = [
+    "serviceAccount:${google_service_account.workload_service.email}"
+  ]
+}
+resource "google_project_iam_binding" "workload_iam_monitoring_viewer" {
+  role = "roles/monitoring.viewer"
+  members = [
+    "serviceAccount:${google_service_account.workload_service.email}"
+  ]
+}
+
+# Creates the k8s service account
+# https://www.terraform.io/docs/providers/kubernetes/r/service_account.html
+resource "kubernetes_service_account" "workload_service" {
+  metadata {
+    name = "workload"
+    annotations = {
+      "iam.gke.io/gcp-service-account" = google_service_account.workload_service.email
+    }
+  }
+}
+
+# Binds the service account to workload account
+# https://www.terraform.io/docs/providers/google/r/google_service_account_iam.html
+resource "google_service_account_iam_member" "workload_iam_member" {
+  service_account_id = google_service_account.workload_service.name
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "serviceAccount:${local.project}.svc.id.goog[default/${kubernetes_service_account.workload_service.metadata[0].name}]"
+}
